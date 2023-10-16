44a3fb68d53fc979e5fb71a001d1bb77c9b270eb
Christopher Diggins <cdiggins@gmail.com>
2023-10-16 14:45:15 -0400

    Fixed console namespace collision

---
+++ Ara3D.Template.Console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ara3D.Template.ConsoleApp
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
